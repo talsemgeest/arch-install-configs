027d27029ba3b6318c6f9cf1c83d20434c8f9ce1
talsemgeest <talsemgeest@gmail.com>
2014-04-20 12:36:37 +1200

    Initial Commit

---
+++ Desktop.sh
@@ -0,0 +1,44 @@
+export EDITOR=nano
+
+echo "en_NZ.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo LANG=en_NZ.UTF-8 > /etc/locale.conf
+export LANG=en_NZ.UTF-8
+
+ln -s /usr/share/zoneinfo/NZ /etc/localtime
+hwclock --systohc --localtime
+
+echo WINTERFELL > /etc/hostname
+systemctl enable dhcpcd.service
+
+mkinitcpio -p linux
+
+pacman -S grub
+chmod -x /etc/grub.d/10_linux
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Set root passwd:"
+passwd
+
+useradd -m -g users -G wheel,games -s /bin/bash talsemgeest
+echo "Set password for talsemgeest"
+passwd talsemgeest
+
+visudo
+
+pacman -S 
+pacman -S smbclient xorg-server xorg-server-utils nvidia lib32-nvidia-libgl ttf-dejavu wget
+
+su talsemgeest <<'EOF'
+mkdir /home/talsemgeest/packages
+cd /home/talsemgeest/packages
+wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz
+tar zxvf packer.tar.gz
+cd packer
+makepkg -s
+EOF
+pacman -U /home/talsemgeest/packages/packer/packer-*-any.pkg.tar.xz
+
+echo "1. Add hostname to /etc/hosts."
+echo "2. grub-install --target=i386-pc --recheck /dev/sdx"
